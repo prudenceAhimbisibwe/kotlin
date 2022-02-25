a4a228a734cbd9b768e2b7a14ab91c1aa275e401
PudenceAhimbisibwe <prudeahimbisibwe@gmail.com>
2022-02-25 07:53:17 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,23 @@
+fun main(){
+    val statement = "The owner of this"
+    val item = "book"
+    val action = "will get it tomorrow"
+    val sentence = statement+ " "+item+ " "+action
+    println(sentence)
+    fruit()
+    x()
+}
+fun fruit(){
+    var fruit = "banana"
+    println(fruit[0])
+    println(fruit[1])
+    println(fruit[2])
+    println(fruit[3])
+    println(fruit[4])
+}
+fun x(){
+    var x ="banana"
+    println(x[0])
+    println(x[1])
+    println(x[2])
+}
